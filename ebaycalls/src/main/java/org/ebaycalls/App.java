42ff859bdb1f199b9cf282284fb0f7229e06795e
lyan2 <lyan2@ebay.com>
2018-09-28 08:07:16 +0800

    initialize

---
+++ ebaycalls/src/main/java/org/ebaycalls/App.java
@@ -0,0 +1,13 @@
+package org.ebaycalls;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
---
+++ ebaysdkcore/src/main/java/org/ebaysdkcore/App.java
@@ -0,0 +1,13 @@
+package org.ebaysdkcore;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
